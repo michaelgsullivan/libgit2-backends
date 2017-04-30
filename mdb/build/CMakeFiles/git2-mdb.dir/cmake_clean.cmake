e5c7a8b28bf8dcbc753d73ea75e76c0ab090c1e6
Mike Sullivan <mgsullivan@gmail.com>
2017-04-30 00:14:24 -0700

    Added mdb dir and some code

---
+++ mdb/build/CMakeFiles/git2-mdb.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git2-mdb.dir/mdb.c.o"
+  "libgit2-mdb.pdb"
+  "libgit2-mdb.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/git2-mdb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
